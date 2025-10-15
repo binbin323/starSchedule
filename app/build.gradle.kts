dd15088e4e85c003b7ad75e3f2b6896ebeab06f9
lightStarrr <2733977932@qq.com>
2025-10-15 16:28:21 +0800

    chore(app): 更新应用版本号- 将 versionCode从 23 更新为 24 - 将 versionName 从1.0.23 更新为 1.0.24

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.star.schedule"
         minSdk = 33
         targetSdk = 36
-        versionCode = 23
-        versionName = "1.0.23"
+        versionCode = 24
+        versionName = "1.0.24"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
